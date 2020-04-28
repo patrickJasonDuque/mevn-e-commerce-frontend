9b27443355a5c46708543c1ce6095a186bb19e62
Patrick Duque <patpatduque26@gmail.com>
2020-04-28 12:38:52 +0800

    removed dist

---
+++ dist/precache-manifest.77c04712d765cb007f5a37e916278bbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a58014fe484afc5c181",
+    "url": "/css/app.5c6ac6e6.css"
+  },
+  {
+    "revision": "2daee7d2194de2de4482",
+    "url": "/css/chunk-vendors.c4e4a2a3.css"
+  },
+  {
+    "revision": "d60aa16182b549831922817b579f60f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a58014fe484afc5c181",
+    "url": "/js/app.70d4152f.js"
+  },
+  {
+    "revision": "2daee7d2194de2de4482",
+    "url": "/js/chunk-vendors.100dda8a.js"
+  },
+  {
+    "revision": "098a474594a27cffebff8034f5be9991",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
